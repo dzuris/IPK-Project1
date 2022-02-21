d85f1df86091b5f66cfa5c2205397a5dbc033ba5
dzuris <adamdzurilla19@gmail.com>
2022-02-21 18:05:23 +0100

    server push
    server listening from browser and displaying "Hello World" message

--- server.c
+++ server.c
@@ -1,78 +1,61 @@
+// Server side C program to demonstrate HTTP Server programming
 #include <stdio.h>
-#include <stdlib.h>
-
 #include <sys/socket.h>
-#include <sys/types.h>
-
+#include <unistd.h>
+#include <stdlib.h>
 #include <netinet/in.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+#define PORT 8080
+int main(int argc, char const *argv[])
 {
-	// check if user passed PORT number
-	if(argc != 2)
-	{
-		fprintf(stderr, "Wrong arguments input!\n");
+    int server_fd, new_socket; long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    // Only this line has been changed. Everything is same.
+    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+    
 
-		return 1;
-	}
-
-	int port = atoi(argv[1]);
-
-	char server_message[256] = "You have reached the server!\n";
-
-	// create the server socket
-	int server_socket;
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	// check if socket was successfully created
-	if(server_socket < 0)
-	{
-		fprintf(stderr, "Cannot create socket\n");
-
-		return 1;
-	}
-
-	// define the server address
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(port);
-	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	// bind the socket to our specified IP and port
-	if(bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address)) < 0)
-	{
-		fprintf(stderr, "Bind failed\n");
-
-		return 1;
-	}
-
-	if (listen(server_socket, 5) < 0)
-	{
-		fprintf(stderr, "Error in listen\n");
-
-		return 1;
-	}
-
-	while (1)
-	{
-		printf("Waiting for new connection\n\n");
-		int client_socket;
-		client_socket = accept(server_socket, NULL, NULL);
-
-		if (client_socket < 0)
-		{
-			fprintf(stderr, "Error in accept\n");
-
-			return 1;
-		}
-
-		// send the message
-		send(client_socket, server_message, sizeof(server_message), 0);
-		printf(server_message);
-
-		// close the socket
-		close(server_socket);
-	}
-
-	return 0;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        
+        char buffer[30000] = {0};
+        valread = read( new_socket , buffer, 30000);
+        printf("%s\n",buffer );
+        write(new_socket , hello , strlen(hello));
+        printf("------------------Hello message sent-------------------");
+        close(new_socket);
+    }
+    return 0;
 }
