98422162bd2e0e26d11ce16f8e9c9cf965db02ba
dzuris <adamdzurilla19@gmail.com>
2022-02-23 19:19:54 +0100

    everything working

--- server.c
+++ server.c
@@ -1,6 +1,7 @@
 // Server side C program to demonstrate HTTP Server programming
 #include <stdio.h>
 #include <sys/socket.h>
+//#include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -90,12 +91,14 @@ int main(int argc, char const *argv[])
         else if(!strncmp(buffer, "GET /load ", 10))
         {
             printf("System returning cpu-load to client\n");
-            create_http_message(message, "vrat load");
+            char percent[4];
+            sprintf(percent, "%d", GetCPULoad());
+            strcat(percent, "%");
+            create_http_message(message, percent);
         }
         else
         {
-            printf("Invalid request by client\n");
-            create_http_message(message, "404: Not Found");
+            create_http_message(message, "400: Bad request");
         }
 
         write(client_socket , message , (int)strlen(message));
@@ -149,7 +152,86 @@ void get_cpu_name(char *return_cpu_name)
     pclose(file);
 }
 
-int get_load()
+int GetCPULoad()
 {
+    unsigned long long int first_data[10];
+    unsigned long long int second_data[10];
+    get_cpu_data(&first_data);
+    usleep(500000);
+    get_cpu_data(&second_data);
 
+    first_data[3] = first_data[3] + first_data[4];
+    second_data[3] = second_data[3] + second_data[4];
+    unsigned long long int prev_non_idle = first_data[0] + first_data[1] + first_data[2] + first_data[5] + first_data[6] + first_data[7];
+    unsigned long long int non_idle = second_data[0] + second_data[1] + second_data[2] + second_data[5] + second_data[6] + second_data[7];
+    unsigned long long int prev_total = first_data[3] + prev_non_idle;
+    unsigned long long int total = second_data[3] + non_idle;
+    unsigned long long int total_diff = total - prev_total;
+    unsigned long long int idle_diff = second_data[3] - first_data[3];
+
+    int load_int = ((total_diff - idle_diff)/(float)total_diff)*100;
+    float load = ((total_diff - idle_diff)/(float)total_diff)*100;
+    if ((load - load_int) >= 0.50)
+        load_int++;
+
+    /*
+    pdata = first
+    cdata = second
+    unsigned long long int user;        0
+    unsigned long long int nice;        1
+    unsigned long long int systemA;     2
+    unsigned long long int idle;        3
+    unsigned long long int iowait;      4
+    unsigned long long int irq;         5
+    unsigned long long int softirq;     6
+    unsigned long long int steal;       7
+    unsigned long long int guest;       8
+    unsigned long long int guest_nice;  9
+    */
+
+    return load_int;
+}
+
+void get_cpu_data(unsigned long long int *data)
+{
+    char cpu_data_str[256];
+    FILE *file = fopen("/proc/stat", "r");
+    fgets(cpu_data_str, 256, file);
+    fclose(file);
+
+    int init_size = strlen(cpu_data_str);
+    char delim[] = " ";
+
+    char *ptr = strtok(cpu_data_str, delim);
+
+    char data_list[11][256];
+    int i = 0;
+
+    while (ptr != NULL)
+    {
+        strcpy(data_list[i++], ptr);
+        ptr = strtok(NULL, delim);
+    }
+
+    data[0] = atoi(data_list[1]);
+    data[1] = atoi(data_list[2]);
+    data[2] = atoi(data_list[3]);
+    data[3] = atoi(data_list[4]);
+    data[4] = atoi(data_list[5]);
+    data[5] = atoi(data_list[6]);
+    data[6] = atoi(data_list[7]);
+    data[7] = atoi(data_list[8]);
+    data[8] = atoi(data_list[9]);
+    data[9] = atoi(data_list[10]);
+
+    /*data->user = data_list[0];
+    data->nice = data_list[1];
+    data->systemA;
+    data->idle;
+    data->iowait;
+    data->irq;
+    data->softirq;
+    data->steal;
+    data->guest;
+    data->guest_nice;*/
 }
--- server.h
+++ server.h
@@ -5,4 +5,5 @@ void create_http_message(char *final_message, char *message_to_include);
 
 void get_hostname(char *return_hostname);
 void get_cpu_name(char *return_cpu_name);
-int get_load();
+int GetCPULoad();
+void get_cpu_data(unsigned long long int *data);
